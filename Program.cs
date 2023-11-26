664a9743a3064399b7e3ad1e9e2ab27aa76cd0da
Hasini Punsala <hasinipunsala@gmail.com>
2023-11-26 23:42:39 +0530

    Create Character and Player classes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOPAdventure;
+
+Console.WriteLine("Hello, what is your name!");
+var name = Console.ReadLine();
+
+if (name == String.Empty)
+    name = "No Name";
+
+var player = new Player(name);
+
+Console.WriteLine("Welcome {0} to your OOP Adventure!", player.Name);
